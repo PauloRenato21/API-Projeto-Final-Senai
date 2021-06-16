8f8174b7fd3f10e5401058eb2c3363ee1f95fcf2
PauloRenato21 <paulorenato401@gmail.com>
2021-06-16 10:18:02 -0300

    add metodo informacoes atleta

--- app/Controllers/Atleta.php
+++ app/Controllers/Atleta.php
@@ -1,6 +1,8 @@
 <?php namespace App\Controllers;
 
 use App\Models\AtletaModel;
+use App\Models\ResponsavelModel;
+use App\Models\TurmaModel;
 use CodeIgniter\RESTful\ResourceController;
 
 class Atleta extends ResourceController{
@@ -17,7 +19,48 @@ class Atleta extends ResourceController{
     //Metodo que tras informações relacionadas com um Atleta: 
     //turma, responsavel.
     public function atletaTurmaResponsavel(){
-    
+        $modelAtleta = new AtletaModel();
+        $modelTurma = new TurmaModel();
+        $modelResponsavel = new ResponsavelModel();
+        
+        $dataAtleta = $modelAtleta->findAll();
+        $dataTurma = $modelTurma->findAll();
+        $dataResponsavel = $modelResponsavel->findAll();
+
+        $resultado = [];
+
+        foreach($dataAtleta as $atleta){
+            $atleta['turma'] = array();
+            $atleta['responsavel'] = array();
+
+            foreach($dataTurma as $turma){
+                if($atleta['fk_turma_id'] == $turma['id']){
+                    $atletaTurma['nome'] = $turma['nome'];
+                    $atletaTurma['turno'] = $turma['turno'];
+                    $atletaTurma['horario_inicial'] = $turma['horario_inicial'];
+                    $atletaTurma['horario_termino'] = $turma['horario_termino'];
+
+                    array_push($atleta['turma'],$atletaTurma);
+                }
+            }
+
+            foreach($dataResponsavel as $responsavel){
+                if($atleta['fk_responsavel'] == $responsavel['id']){
+                    $atletaRespon['nome'] = $responsavel['nome'];
+                    $atletaRespon['cpf'] = $responsavel['cpf'];
+                    $atletaRespon['grau_parentesco'] = $responsavel['grau_parentesco'];
+                    $atletaRespon['profissao'] = $responsavel['profissao'];
+                    $atletaRespon['local_trabalho'] = $responsavel['local_trabalho'];
+                    $atletaRespon['telefone'] = $responsavel['telefone'];
+                    $atletaRespon['email'] = $responsavel['email'];
+
+                    array_push($atleta['responsavel'],$atletaRespon);
+                }
+            }
+            array_push($resultado,$atleta);
+        }
+
+        return $this->respond($resultado);
     }
 
     //Metodo Insert Atleta.
