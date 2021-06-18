82048c4db0368c88371030f29c78299dc6b2a5f0
PauloRenato21 <paulorenato401@gmail.com>
2021-06-18 16:26:44 -0300

    add validation token metodos categoria

--- app/Controllers/Categoria.php
+++ app/Controllers/Categoria.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controllers;
- 
+
+use App\Helpers\Validacao;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\CategoriaModel;
@@ -7,86 +8,174 @@ use App\Models\CategoriaModel;
 class Categoria extends ResourceController
 {
     use ResponseTrait;
+
+    public $validacao;
+
+    public function __construct()
+    {
+        $this->validacao = new Validacao();
+    }
+
     // lista todos responsaveis
     public function index()
     {
-        $model = new CategoriaModel();
-        $data = $model->findAll();
-        return $this->respond($data);
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new CategoriaModel();
+                $data = $model->findAll();
+                return $this->respond($data);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
+        }
     }
  
     // lista um Categorias
     public function show($id = null)
     {
-        $model = new CategoriaModel();
-        $data = $model->getWhere(['id' => $id])->getResult();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($data){
-            return $this->respond($data);
+                $model = new CategoriaModel();
+                $data = $model->getWhere(['id' => $id])->getResult();
+
+                if($data){
+                    return $this->respond($data);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
  
     // adiciona um Categorias
     public function create()
     {
-        $model = new CategoriaModel();
-        $data = $this->request->getJSON();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($model->insert($data)){
-            $response = [
-                'status'   => 201,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados salvos'
-                ]
+                $model = new CategoriaModel();
+                $data = $this->request->getJSON();
+
+                if($model->insert($data)){
+                    $response = [
+                        'status'   => 201,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados salvos'
+                        ]
+                    ];
+                    return $this->respondCreated($response);
+                }
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondCreated($response);
+            return $this->respond($erro);
         }
-
-        return $this->fail($model->errors());
     }
     
     // atualiza um Categorias
     public function update($id = null)
     {
-        $model = new CategoriaModel();
-        $data = $this->request->getJSON();
-        
-        if($model->update($id, $data)){
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados atualizados'
-                    ]
-                ];
-                return $this->respond($response);
-            };
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-            return $this->fail($model->errors());
+                $model = new CategoriaModel();
+                $data = $this->request->getJSON();
+                
+                if($model->update($id, $data)){
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados atualizados'
+                            ]
+                        ];
+                        return $this->respond($response);
+                    };
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
+    }
  
     // deleta um Categorias
     public function delete($id = null)
     {
-        $model = new CategoriaModel();
-        $data = $model->find($id);
-        
-        if($data){
-            $model->delete($id);
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados removidos'
-                ]
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new CategoriaModel();
+                $data = $model->find($id);
+                
+                if($data){
+                    $model->delete($id);
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados removidos'
+                        ]
+                    ];
+                    return $this->respondDeleted($response);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondDeleted($response);
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
  
 }
