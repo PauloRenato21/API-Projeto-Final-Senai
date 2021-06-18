f7d80df3331ba06b51a7be718744757168483313
PauloRenato21 <paulorenato401@gmail.com>
2021-06-18 16:26:19 -0300

    add validation token metodos cargo

--- app/Controllers/Cargo.php
+++ app/Controllers/Cargo.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controllers;
- 
+
+use App\Helpers\Validacao;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\CargoModel;
@@ -8,114 +9,218 @@ use App\Models\FuncionarioModel;
 class Cargo extends ResourceController
 {
     use ResponseTrait;
+
+    public $validacao;
+
+    public function __construct()
+    {
+        $this->validacao = new Validacao();
+    }
+
     // lista todos responsaveis
     public function index()
     {
-        $model = new CargoModel();
-        $data = $model->findAll();
-        return $this->respond($data);
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new CargoModel();
+                $data = $model->findAll();
+                return $this->respond($data);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
+        }
     }
  
     // lista um Cargos
     public function show($id = null)
     {
-        $model = new CargoModel();
-        $data = $model->getWhere(['id' => $id])->getResult();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($data){
-            return $this->respond($data);
+                $model = new CargoModel();
+                $data = $model->getWhere(['id' => $id])->getResult();
+
+                if($data){
+                    return $this->respond($data);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
  
     // adiciona um Cargos
     public function create()
     {
-        $model = new CargoModel();
-        $data = $this->request->getJSON();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($model->insert($data)){
-            $response = [
-                'status'   => 201,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados salvos'
-                ]
+                $model = new CargoModel();
+                $data = $this->request->getJSON();
+
+                if($model->insert($data)){
+                    $response = [
+                        'status'   => 201,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados salvos'
+                        ]
+                    ];
+                    return $this->respondCreated($response);
+                }
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondCreated($response);
+            return $this->respond($erro);
         }
-
-        return $this->fail($model->errors());
     }
     
     // atualiza um Cargos
     public function update($id = null)
     {
-        $model = new CargoModel();
-        $data = $this->request->getJSON();
-        
-        if($model->update($id, $data)){
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados atualizados'
-                    ]
-                ];
-                return $this->respond($response);
-            };
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-            return $this->fail($model->errors());
+                $model = new CargoModel();
+                $data = $this->request->getJSON();
+                
+                if($model->update($id, $data)){
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados atualizados'
+                            ]
+                        ];
+                    return $this->respond($response);
+                };
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
+    }
  
     // deleta um Cargos
     public function delete($id = null)
     {
-        $model = new CargoModel();
-        $data = $model->find($id);
-        
-        if($data){
-            $model->delete($id);
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados removidos'
-                ]
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new CargoModel();
+                $data = $model->find($id);
+                
+                if($data){
+                    $model->delete($id);
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados removidos'
+                        ]
+                    ];
+                    return $this->respondDeleted($response);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondDeleted($response);
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
 
     public function cargofuncionario(){
-        $funcionarioModel = new FuncionarioModel();
-        $cargoModel = new CargoModel();
-        
-        $datafuncionario = $funcionarioModel->findAll();
-        $datacargo = $cargoModel->findAll();
-        
-        $resultado = [];
-        
-        foreach ($datafuncionario as $funcionario){
-            $funcionario['cargo'] = array();
-        
-            foreach ($datacargo as $cl){
-                if ($funcionario['fk_cargo_id'] == $cl['id']){
-        
-                    $funcionariocargo['id'] = $cl['id'];
-                    $funcionariocargo['nome'] = $cl['nome'];
-        
-                    array_push($funcionario['cargo'], $funcionariocargo);
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $funcionarioModel = new FuncionarioModel();
+                $cargoModel = new CargoModel();
+                
+                $datafuncionario = $funcionarioModel->findAll();
+                $datacargo = $cargoModel->findAll();
+                
+                $resultado = [];
+                
+                foreach ($datafuncionario as $funcionario){
+                    $funcionario['cargo'] = array();
+                
+                    foreach ($datacargo as $cl){
+                        if ($funcionario['fk_cargo_id'] == $cl['id']){
+                
+                            $funcionariocargo['id'] = $cl['id'];
+                            $funcionariocargo['nome'] = $cl['nome'];
+                
+                            array_push($funcionario['cargo'], $funcionariocargo);
+                        }
+                    }
+                
+                    array_push($resultado, $funcionario);
                 }
+                
+                return $this->respond($resultado);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
             }
-        
-            array_push($resultado, $funcionario);
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->respond($resultado);
     }
  
 }
