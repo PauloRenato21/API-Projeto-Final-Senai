f6eb02219d7a6343437402972e0acc7ff10b88a8
PauloRenato21 <paulorenato401@gmail.com>
2021-06-18 16:27:24 -0300

    add validation token metodos funcionario

--- app/Controllers/Funcionario.php
+++ app/Controllers/Funcionario.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controllers;
- 
+
+use App\Helpers\Validacao;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\FuncionarioModel;
@@ -11,179 +12,299 @@ use App\Models\TurmaFuncionarioModel;
 class Funcionario extends ResourceController
 {
     use ResponseTrait;
+
+    public $validacao;
+
+    public function __construct()
+    {
+        $this->validacao = new Validacao();
+    }
+
     // lista todos Funcionario
     public function index()
     {
-        $model = new FuncionarioModel();
-        $data = $model->findAll();
-        return $this->respond($data);
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new FuncionarioModel();
+                $data = $model->findAll();
+                return $this->respond($data);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
+        }
     }
  
     // lista um Funcionario
     public function show($id = null)
     {
-        $model = new FuncionarioModel();
-        $data = $model->getWhere(['id' => $id])->getResult();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($data){
-            return $this->respond($data);
+                $model = new FuncionarioModel();
+                $data = $model->getWhere(['id' => $id])->getResult();
+
+                if($data){
+                    return $this->respond($data);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
  
     // adiciona um Funcionario
     public function create()
     {
-        $model = new FuncionarioModel();
-        $data = $this->request->getJSON();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($model->insert($data)){
-            $response = [
-                'status'   => 201,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados salvos'
-                ]
+                $model = new FuncionarioModel();
+                $data = $this->request->getJSON();
+
+                if($model->insert($data)){
+                    $response = [
+                        'status'   => 201,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados salvos'
+                        ]
+                    ];
+                    return $this->respondCreated($response);
+                }
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondCreated($response);
+            return $this->respond($erro);
         }
-
-        return $this->fail($model->errors());
     }
     
     // atualiza um Funcionario
     public function update($id = null)
     {
-        $model = new FuncionarioModel();
-        $data = $this->request->getJSON();
-        
-        if($model->update($id, $data)){
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados atualizados'
-                    ]
-                ];
-                return $this->respond($response);
-            };
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-            return $this->fail($model->errors());
+                $model = new FuncionarioModel();
+                $data = $this->request->getJSON();
+                
+                if($model->update($id, $data)){
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados atualizados'
+                            ]
+                        ];
+                        return $this->respond($response);
+                    };
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
+    }
  
     // deleta um Funcionario
     public function delete($id = null)
     {
-        $model = new FuncionarioModel();
-        $data = $model->find($id);
-        
-        if($data){
-            $model->delete($id);
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados removidos'
-                ]
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new FuncionarioModel();
+                $data = $model->find($id);
+                
+                if($data){
+                    $model->delete($id);
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados removidos'
+                        ]
+                    ];
+                    return $this->respondDeleted($response);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondDeleted($response);
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
 
     public function funcionariocargofranquia(){
-        $funcionarioModel = new FuncionarioModel();
-        $cargoModel = new CargoModel();
-        $franquiaModel = new FranquiaModel();
-        
-        $datafuncionario = $funcionarioModel->findAll();
-        $datacargo = $cargoModel->findAll();
-        $datafranquia = $franquiaModel->findAll();
-        
-        $resultado = [];
-        
-        foreach ($datafuncionario as $funcionario){
-            $funcionario['cargo'] = array();
-            $funcionario['franquia'] = array();
-        
-            foreach ($datacargo as $cl){
-                if ($funcionario['fk_cargo_id'] == $cl['id']){
-        
-                    $funcionariocargo['id'] = $cl['id'];
-                    $funcionariocargo['nome'] = $cl['nome'];
-        
-                    array_push($funcionario['cargo'], $funcionariocargo);
-                }
-            }
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-            foreach($datafranquia as $franquia){
-                if($funcionario['fk_franquias_id'] == $franquia['id']){
-                    $funcionariofranquia['nome'] = $franquia['nome'];
-                    $funcionariofranquia['cnpj'] = $franquia['cnpj'];
-                    $funcionariofranquia['rua'] = $franquia['endereco_rua'];
-                    $funcionariofranquia['numero'] = $franquia['endereco_numero'];
-                    $funcionariofranquia['cep'] = $franquia['endereco_CEP'];
-                    $funcionariofranquia['estado'] = $franquia['estado'];
-                    $funcionariofranquia['cidade'] = $franquia['cidade'];
-                    $funcionariofranquia['telefone'] = $franquia['telefone'];
-                    $funcionariofranquia['email'] = $franquia['email'];
+                $funcionarioModel = new FuncionarioModel();
+                $cargoModel = new CargoModel();
+                $franquiaModel = new FranquiaModel();
+                
+                $datafuncionario = $funcionarioModel->findAll();
+                $datacargo = $cargoModel->findAll();
+                $datafranquia = $franquiaModel->findAll();
+                
+                $resultado = [];
+                
+                foreach ($datafuncionario as $funcionario){
+                    $funcionario['cargo'] = array();
+                    $funcionario['franquia'] = array();
+                
+                    foreach ($datacargo as $cl){
+                        if ($funcionario['fk_cargo_id'] == $cl['id']){
+                
+                            $funcionariocargo['id'] = $cl['id'];
+                            $funcionariocargo['nome'] = $cl['nome'];
+                
+                            array_push($funcionario['cargo'], $funcionariocargo);
+                        }
+                    }
 
-                    array_push($funcionario['franquia'],$funcionariofranquia);
+                    foreach($datafranquia as $franquia){
+                        if($funcionario['fk_franquias_id'] == $franquia['id']){
+                            $funcionariofranquia['nome'] = $franquia['nome'];
+                            $funcionariofranquia['cnpj'] = $franquia['cnpj'];
+                            $funcionariofranquia['rua'] = $franquia['endereco_rua'];
+                            $funcionariofranquia['numero'] = $franquia['endereco_numero'];
+                            $funcionariofranquia['cep'] = $franquia['endereco_CEP'];
+                            $funcionariofranquia['estado'] = $franquia['estado'];
+                            $funcionariofranquia['cidade'] = $franquia['cidade'];
+                            $funcionariofranquia['telefone'] = $franquia['telefone'];
+                            $funcionariofranquia['email'] = $franquia['email'];
+
+                            array_push($funcionario['franquia'],$funcionariofranquia);
+                        }
+                    }
+                
+                    array_push($resultado, $funcionario);
                 }
+                
+                return $this->respond($resultado);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
             }
-        
-            array_push($resultado, $funcionario);
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->respond($resultado);
     }
 
     public function funcionariocargoturma(){
-        $funcionarioModel = new FuncionarioModel();
-        $cargoModel = new CargoModel();
-        $turmafuncionarioModel = new TurmaFuncionarioModel();
-        
-        $datafuncionario = $funcionarioModel->findAll();
-        $datacargo = $cargoModel->findAll();
-        $dataturmafuncionario = $turmafuncionarioModel->findAll();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        $allDataTurmaFuncionario = $turmafuncionarioModel->getAllTurmaFuncionario()->findAll();
-        
-        $resultado = [];
-        
-        foreach ($datafuncionario as $funcionario){
-            $funcionario['cargo'] = array();
-            $funcionario['turma'] = array();
+                $funcionarioModel = new FuncionarioModel();
+                $cargoModel = new CargoModel();
+                $turmafuncionarioModel = new TurmaFuncionarioModel();
+                
+                $datafuncionario = $funcionarioModel->findAll();
+                $datacargo = $cargoModel->findAll();
+                $dataturmafuncionario = $turmafuncionarioModel->findAll();
 
-            foreach ($datacargo as $cl){
-                if ($funcionario['fk_cargo_id'] == $cl['id']){
-        
-                    $funcionariocargo['id'] = $cl['id'];
-                    $funcionariocargo['nome'] = $cl['nome'];
-        
-                    array_push($funcionario['cargo'], $funcionariocargo);
+                $allDataTurmaFuncionario = $turmafuncionarioModel->getAllTurmaFuncionario()->findAll();
+                
+                $resultado = [];
+                
+                foreach ($datafuncionario as $funcionario){
+                    $funcionario['cargo'] = array();
+                    $funcionario['turma'] = array();
+
+                    foreach ($datacargo as $cl){
+                        if ($funcionario['fk_cargo_id'] == $cl['id']){
+                
+                            $funcionariocargo['id'] = $cl['id'];
+                            $funcionariocargo['nome'] = $cl['nome'];
+                
+                            array_push($funcionario['cargo'], $funcionariocargo);
+                        }
+                    }
+                
+                    foreach ($allDataTurmaFuncionario as $tf){
+                        if ($funcionario['id'] == $tf['fk_funcionario_id']){
+                    
+                            $funcionarioturma['id'] = $tf['fk_turma_id'];
+                            $funcionarioturma['nome'] = $tf['nome'];
+                            $funcionarioturma['turno'] = $tf['turno'];
+                            $funcionarioturma['horario_inicial'] = $tf['horario_inicial'];
+                            $funcionarioturma['horario_termino'] = $tf['horario_termino'];
+                    
+                            array_push($funcionario['turma'], $funcionarioturma);
+                        }
+                    }
+                
+                    array_push($resultado, $funcionario);
                 }
+                
+                return $this->respond($resultado);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
             }
-        
-            foreach ($allDataTurmaFuncionario as $tf){
-                if ($funcionario['id'] == $tf['fk_funcionario_id']){
-            
-                    $funcionarioturma['id'] = $tf['fk_turma_id'];
-                    $funcionarioturma['nome'] = $tf['nome'];
-                    $funcionarioturma['turno'] = $tf['turno'];
-                    $funcionarioturma['horario_inicial'] = $tf['horario_inicial'];
-                    $funcionarioturma['horario_termino'] = $tf['horario_termino'];
-            
-                    array_push($funcionario['turma'], $funcionarioturma);
-                }
-            }
-        
-            array_push($resultado, $funcionario);
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->respond($resultado);
     }
  
 }
