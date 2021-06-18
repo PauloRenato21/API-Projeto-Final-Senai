7a6be7897cb7e48651dd14d4b228e6b05e2e769d
PauloRenato21 <paulorenato401@gmail.com>
2021-06-18 16:27:39 -0300

    add validation token metodos responsavel

--- app/Controllers/Responsavel.php
+++ app/Controllers/Responsavel.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controllers;
- 
+
+use App\Helpers\Validacao;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\ResponsavelModel;
@@ -9,146 +10,245 @@ use App\Models\TurmaModel;
 class Responsavel extends ResourceController
 {
     use ResponseTrait;
+
+    public $validacao;
+
+    public function __construct()
+    {
+        $this->validacao = new Validacao();
+    }
+
     // lista todos responsaveis
     public function index()
     {
-        $model = new ResponsavelModel();
-        $data = $model->findAll();
-        return $this->respond($data);
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new ResponsavelModel();
+                $data = $model->findAll();
+                return $this->respond($data);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
+        }
     }
  
     // lista um responsavels
     public function show($id = null)
     {
-        $model = new ResponsavelModel();
-        $data = $model->getWhere(['id' => $id])->getResult();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($data){
-            return $this->respond($data);
+                $model = new ResponsavelModel();
+                $data = $model->getWhere(['id' => $id])->getResult();
+
+                if($data){
+                    return $this->respond($data);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
  
     // adiciona um responsavels
     public function create()
     {
-        $model = new ResponsavelModel();
-        $data = $this->request->getJSON();
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-        if($model->insert($data)){
-            $response = [
-                'status'   => 201,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados salvos'
-                ]
+                $model = new ResponsavelModel();
+                $data = $this->request->getJSON();
+
+                if($model->insert($data)){
+                    $response = [
+                        'status'   => 201,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados salvos'
+                        ]
+                    ];
+                    return $this->respondCreated($response);
+                }
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondCreated($response);
+            return $this->respond($erro);
         }
-
-        return $this->fail($model->errors());
     }
     
     // atualiza um responsavels
     public function update($id = null)
     {
-        $model = new ResponsavelModel();
-        $data = $this->request->getJSON();
-        
-        if($model->update($id, $data)){
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados atualizados'
-                    ]
-                ];
-                return $this->respond($response);
-            };
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-            return $this->fail($model->errors());
+                $model = new ResponsavelModel();
+                $data = $this->request->getJSON();
+                
+                if($model->update($id, $data)){
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados atualizados'
+                            ]
+                        ];
+                    return $this->respond($response);
+                };
+
+                return $this->fail($model->errors());
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
         }
+    }
  
     // deleta um responsavels
     public function delete($id = null)
     {
-        $model = new ResponsavelModel();
-        $data = $model->find($id);
-        
-        if($data){
-            $model->delete($id);
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Dados removidos'
-                ]
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
+
+                $model = new ResponsavelModel();
+                $data = $model->find($id);
+                
+                if($data){
+                    $model->delete($id);
+                    $response = [
+                        'status'   => 200,
+                        'error'    => null,
+                        'messages' => [
+                            'success' => 'Dados removidos'
+                        ]
+                    ];
+                    return $this->respondDeleted($response);
+                }
+                
+                return $this->failNotFound('Nenhum dado encontrado com id '.$id);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
             ];
-            return $this->respondDeleted($response);
+            return $this->respond($erro);
         }
-        
-        return $this->failNotFound('Nenhum dado encontrado com id '.$id);        
     }
 
     public function responsavelatletaturma(){
-        $responsavelModel = new ResponsavelModel();
-        $atletaModel = new AtletaModel();
-        $turmaModel = new TurmaModel();
-        
-        $dataresponsavel = $responsavelModel->findAll();
-        $dataatleta = $atletaModel->findAll();
-        $dataturma = $turmaModel->findAll();
-        
-        $resultado = [];
-        
-        foreach ($dataresponsavel as $responsavel){
-            $responsavel['atleta'] = array();
-            $responsavel['turma'] = array();
-        
-            foreach ($dataatleta as $at){
-                if ($responsavel['id'] == $at['fk_Responsavel_id']){
-        
-                    $responsavelatleta['id'] = $at['id'];
-                    $responsavelatleta['nome'] = $at['nome'];
-                    $responsavelatleta['cpf'] = $at['cpf'];
-                    $responsavelatleta['dt_nascimento'] = $at['dt_nascimento'];
-                    $responsavelatleta['endereco_rua'] = $at['endereco_rua'];
-                    $responsavelatleta['endereco_numero'] = $at['endereco_numero'];
-                    $responsavelatleta['endereco_bairro'] = $at['endereco_bairro'];
-                    $responsavelatleta['endereco_CEP'] = $at['endereco_CEP'];
-                    $responsavelatleta['naturalidade'] = $at['naturalidade'];
-                    $responsavelatleta['problema_saude'] = $at['problema_saude'];
-                    $responsavelatleta['alergia'] = $at['alergia'];
-                    $responsavelatleta['medicamento'] = $at['medicamento'];
-                    $responsavelatleta['telefone'] = $at['telefone'];
-                    $responsavelatleta['email'] = $at['email'];
-                    $id_atleta = $at['fk_turma_id'];
-                    array_push($responsavel['atleta'], $responsavelatleta);
-                }
-            }
+        if($this->request->getHeader("Authorization")){
+            if($this->validacao->validacaoToken($this->request->getHeader("Authorization")->getValue())){
 
-            
+                $responsavelModel = new ResponsavelModel();
+                $atletaModel = new AtletaModel();
+                $turmaModel = new TurmaModel();
                 
-            
-                foreach ($dataturma as $tur){
-                    if ($id_atleta == $tur['id']){
-    
-                        $atletaturma['id'] = $tur['id'];
-                        $atletaturma['nome'] = $tur['nome'];
-                        $atletaturma['turno'] = $tur['turno'];
-                        $atletaturma['horario_inicial'] = $tur['horario_inicial'];
-                        $atletaturma['horario_termino'] = $tur['horario_termino'];
-            
-                        array_push($responsavel['turma'], $atletaturma);
+                $dataresponsavel = $responsavelModel->findAll();
+                $dataatleta = $atletaModel->findAll();
+                $dataturma = $turmaModel->findAll();
+                
+                $resultado = [];
+                
+                foreach ($dataresponsavel as $responsavel){
+                    $responsavel['atleta'] = array();
+                    $responsavel['turma'] = array();
+                
+                    foreach ($dataatleta as $at){
+                        if ($responsavel['id'] == $at['fk_Responsavel_id']){
+                
+                            $responsavelatleta['id'] = $at['id'];
+                            $responsavelatleta['nome'] = $at['nome'];
+                            $responsavelatleta['cpf'] = $at['cpf'];
+                            $responsavelatleta['dt_nascimento'] = $at['dt_nascimento'];
+                            $responsavelatleta['endereco_rua'] = $at['endereco_rua'];
+                            $responsavelatleta['endereco_numero'] = $at['endereco_numero'];
+                            $responsavelatleta['endereco_bairro'] = $at['endereco_bairro'];
+                            $responsavelatleta['endereco_CEP'] = $at['endereco_CEP'];
+                            $responsavelatleta['naturalidade'] = $at['naturalidade'];
+                            $responsavelatleta['problema_saude'] = $at['problema_saude'];
+                            $responsavelatleta['alergia'] = $at['alergia'];
+                            $responsavelatleta['medicamento'] = $at['medicamento'];
+                            $responsavelatleta['telefone'] = $at['telefone'];
+                            $responsavelatleta['email'] = $at['email'];
+                            $id_atleta = $at['fk_turma_id'];
+                            array_push($responsavel['atleta'], $responsavelatleta);
+                        }
                     }
-                }
-                array_push($resultado, $responsavel);
-            
-            
-        }
 
-        return $this->respond($resultado);
+                    foreach ($dataturma as $tur){
+                        if ($id_atleta == $tur['id']){
+
+                            $atletaturma['id'] = $tur['id'];
+                            $atletaturma['nome'] = $tur['nome'];
+                            $atletaturma['turno'] = $tur['turno'];
+                            $atletaturma['horario_inicial'] = $tur['horario_inicial'];
+                            $atletaturma['horario_termino'] = $tur['horario_termino'];
+                
+                            array_push($responsavel['turma'], $atletaturma);
+                        }
+                    }
+                    array_push($resultado, $responsavel);
+                }
+
+                return $this->respond($resultado);
+    
+            } else{
+                $erro = [
+                    'Erro' => 'Token Inválido'
+                ];
+                return $this->respond($erro);
+            }
+        } else{
+            $erro = [
+                'Erro' => 'Authorization|Token não encontrado'
+            ];
+            return $this->respond($erro);
+        }
     }
  
 }
