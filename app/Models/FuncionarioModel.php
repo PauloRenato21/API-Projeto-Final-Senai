96b65faa36012868025acead12f155e426eb62a1
PauloRenato21 <paulorenato401@gmail.com>
2021-06-21 20:23:34 -0300

    add campos fk_franquias_id e fk_cargo_id

--- app/Models/FuncionarioModel.php
+++ app/Models/FuncionarioModel.php
@@ -6,17 +6,19 @@ class FuncionarioModel extends Model
 {
     protected $table = 'funcionario';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['nome','dt_nascimento', 'cpf', 'rg', 'naturalidade', 'endereco_rua', 'endereco_numero', 'endereco_bairro', 'telefone', 'email'];
+    protected $allowedFields = ['nome','dt_nascimento', 'cpf', 'rg', 'naturalidade', 'endereco_rua', 'endereco_numero', 'endereco_bairro', 'telefone', 'email','fk_franquias_id','fk_cargo_id'];
     protected $validationRules    = [
-        'nome'     => 'required|max_length[45]',
-        'dt_nascimento'         => 'valid_date[Y/m/d]',
-        'cpf'         => 'required|decimal',
-        'rg'         => 'required|decimal', 
-        'naturalidade'     => 'required|max_length[45]',
-        'endereco_rua'         => 'required|max_length[45]',
-        'endereco_numero'         => 'required|max_length[45]',
-        'endereco_bairro'         => 'required|max_length[45]', 
-        'telefone'     => 'required|decimal',
-        'email'         => 'required|max_length[45]'      
+        'nome' => 'required|max_length[45]',
+        'dt_nascimento' => 'valid_date[Y/m/d]',
+        'cpf' => 'required|decimal',
+        'rg' => 'required|decimal', 
+        'naturalidade' => 'required|max_length[45]',
+        'endereco_rua' => 'required|max_length[45]',
+        'endereco_numero' => 'required|max_length[45]',
+        'endereco_bairro' => 'required|max_length[45]', 
+        'telefone' => 'required|decimal',
+        'email' => 'required|max_length[45]',
+        'fk_franquias_id' => 'required|is_natural',
+        'fk_cargo_id' => 'required|is_natural'      
     ];
 }
